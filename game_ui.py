15cbff89ab3c32883683a4247a9dafaa2f68c51c
Amal K <amal.satheesh93@gmail.com>
2020-01-31 17:45:22 +0530

    snake class created. Logic to extend snake in progress

--- game_ui.py
+++ game_ui.py
@@ -10,17 +10,54 @@ clock = pygame.time.Clock()
 
 class Cube():
     def __init__(self, color=(255, 0, 0), size=cell_size, pos=None):
+        self.dirnx = 1
+        self.dirny = 1
         if pos is None:
-            pos = [250, 250]
+            pos = [250, 225]
         self.color = color
         self.size = size
         self.pos = pos
 
-    def create_cube(self, surf):
+    def create_cube(self, surf, dx=1, dy=1):
         x = self.pos[0]
         y = self.pos[1]
-        print(x, x+1,  y, y+1)
-        pygame.draw.rect(surf, self.color, (x, y, self.size, self.size))
+        # print(dy*self.size, dx*self.size)
+        pygame.draw.rect(surf, self.color, (x, y, dx*self.size, dy*self.size))
+
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+
+    def move_snake(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+
+class Snake(Cube):
+    def __init__(self, body=None, color=(0, 0, 255), size=cell_size, pos=None):
+        self.head = Cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        super().__init__(color, size, pos)
+
+    def eat_snack(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
+        if dx == 1 and dy == 0:
+            self.body.append(Cube((tail.pos[0] - 1, tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(Cube((tail.pos[0] + 1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(Cube((tail.pos[0], tail.pos[1] - 1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(Cube((tail.pos[0], tail.pos[1] + 1)))
+
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
+
+
 
 
 def draw_grids(wid=width, size=cell_size):
@@ -34,9 +71,9 @@ def draw_grids(wid=width, size=cell_size):
 def generate_snack_cube(cube_obj, surf,  wid=width):
     x_pos = random.randrange(0, wid, cube_obj.size)
     y_pos = random.randrange(0, wid, cube_obj.size)
+    # x_pos, y_pos = 250, 225
     cube_obj.pos = [x_pos,y_pos]
     cube_obj.create_cube(surf)
-
 
 
 while flag:
@@ -44,7 +81,13 @@ while flag:
     draw_grids()
     snack = Cube()
     generate_snack_cube(snack, win)
-    pygame.display.update()
     pygame.time.delay(50)
     clock.tick(10)
-    flag = False
+    snake = Snake()
+    snake.create_cube(win, dx=snake.body[0].dirnx, dy=snake.body[0].dirny)
+    if snake.body[0].pos == snake.pos:
+        snake.eat_snack()
+        # print(snake.body[-1].dirnx)
+        snake.create_cube(win, dx=snake.body[-1].dirnx, dy=snake.body[-1].dirny)
+    pygame.display.update()
+    snack.move_snake()
