a70a6e6139f2ddfd4c7bb06e1da48a607fa61571
Amal K <amal.satheesh93@gmail.com>
2020-02-14 16:56:55 +0530

    snake started to move. Snack generated randomly.

--- game_ui.py
+++ game_ui.py
@@ -4,6 +4,7 @@ import pygame
 width = 500
 cell_size = 25
 flag = True
+recook_snack = True
 win = pygame.display.set_mode((width, width))
 clock = pygame.time.Clock()
 
@@ -21,8 +22,8 @@ class Cube():
     def create_cube(self, surf, dx=1, dy=1):
         x = self.pos[0]
         y = self.pos[1]
-        # print(dy*self.size, dx*self.size)
-        pygame.draw.rect(surf, self.color, (x, y, dx*self.size, dy*self.size))
+        print(x, y)
+        pygame.draw.rect(surf, self.color, (x, y, dx * self.size, dy * self.size))
 
     def move(self, dirnx, dirny):
         self.dirnx = dirnx
@@ -37,27 +38,28 @@ class Cube():
 
 class Snake(Cube):
     def __init__(self, body=None, color=(0, 0, 255), size=cell_size, pos=None):
-        self.head = Cube(pos)
+        self.head = Cube(color=color, pos=pos)
         self.body = []
         self.body.append(self.head)
         super().__init__(color, size, pos)
 
     def eat_snack(self):
+        global recook_snack
         tail = self.body[-1]
-        dx, dy = tail.dirnx, tail.dirny
-        if dx == 1 and dy == 0:
-            self.body.append(Cube((tail.pos[0] - 1, tail.pos[1])))
-        elif dx == -1 and dy == 0:
-            self.body.append(Cube((tail.pos[0] + 1, tail.pos[1])))
-        elif dx == 0 and dy == 1:
-            self.body.append(Cube((tail.pos[0], tail.pos[1] - 1)))
-        elif dx == 0 and dy == -1:
-            self.body.append(Cube((tail.pos[0], tail.pos[1] + 1)))
-
-        self.body[-1].dirnx = dx
-        self.body[-1].dirny = dy
-
-
+        # dx, dy = tail.dirnx, tail.dirny
+        # if dx == 1 and dy == 0:
+        #     self.body.append(Cube((tail.pos[0] - 1, tail.pos[1])))
+        # elif dx == -1 and dy == 0:
+        #     self.body.append(Cube((tail.pos[0] + 1, tail.pos[1])))
+        # elif dx == 0 and dy == 1:
+        #     self.body.append(Cube((tail.pos[0], tail.pos[1] - 1)))
+        # elif dx == 0 and dy == -1:
+        #     self.body.append(Cube((tail.pos[0], tail.pos[1] + 1)))
+        #
+        # self.body[-1].dirnx = dx
+        # self.body[-1].dirny = dy
+        recook_snack = True
+        self.body.append(Cube(color=(0, 0, 255), pos=[tail.pos[0], tail.pos[1] - cell_size]))
 
 
 def draw_grids(wid=width, size=cell_size):
@@ -68,26 +70,34 @@ def draw_grids(wid=width, size=cell_size):
         pygame.draw.line(win, (0, 0, 0), (0, i), (wid, i), 1)
 
 
-def generate_snack_cube(cube_obj, surf,  wid=width):
-    x_pos = random.randrange(0, wid, cube_obj.size)
-    y_pos = random.randrange(0, wid, cube_obj.size)
-    # x_pos, y_pos = 250, 225
-    cube_obj.pos = [x_pos,y_pos]
+def generate_snack_cube(cube_obj, surf, wid=width):
+    global recook_snack
+    if recook_snack:
+        x_pos = random.randrange(0, wid, cube_obj.size)
+        y_pos = random.randrange(0, wid, cube_obj.size)
+        # x_pos, y_pos = 250, 225
+        cube_obj.pos = [x_pos, y_pos]
     cube_obj.create_cube(surf)
+    recook_snack = False
 
-
+snake = Snake()
+snack = Cube()
 while flag:
     win.fill((255, 255, 255))
     draw_grids()
-    snack = Cube()
     generate_snack_cube(snack, win)
     pygame.time.delay(50)
     clock.tick(10)
-    snake = Snake()
-    snake.create_cube(win, dx=snake.body[0].dirnx, dy=snake.body[0].dirny)
-    if snake.body[0].pos == snake.pos:
+
+    for i in snake.body:
+        i.create_cube(win, dx=snake.body[-1].dirnx, dy=snake.body[-1].dirny)
+        if i.pos[1] < width:
+            i.pos[1] = i.pos[1] + cell_size
+        else:
+            i.pos[1] = 0
+
+    if snake.body[0].pos == snack.pos:
         snake.eat_snack()
         # print(snake.body[-1].dirnx)
-        snake.create_cube(win, dx=snake.body[-1].dirnx, dy=snake.body[-1].dirny)
     pygame.display.update()
     snack.move_snake()
